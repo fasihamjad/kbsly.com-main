7e448a654d3c618675e83a95d9ffe84a30eda808
fasihamjad <fasih000amjad@gmail.com>
2022-10-11 16:24:58 +0500

    yu
    yo

---
+++ client/components/Footer.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useEffect } from "react";
+import getConfig from "next/config";
+
+import showRecaptcha from "../helpers/recaptcha";
+import { useStoreState } from "../store";
+import { ColCenter } from "./Layout";
+import ReCaptcha from "./ReCaptcha";
+import ALink from "./ALink";
+import Text from "./Text";
+
+const { publicRuntimeConfig } = getConfig();
+
+const Footer: FC = () => {
+  const { isAuthenticated } = useStoreState(s => s.auth);
+
+  useEffect(() => {
+    showRecaptcha();
+  }, []);
+
+  return (
+    <ColCenter
+      as="footer"
+      width={1}
+      backgroundColor="white"
+      p={isAuthenticated ? 2 : 24}
+    >
+      {!isAuthenticated && <ReCaptcha />}
+      <Text fontSize={[12, 13]} py={2}>
+        Copyright 2022, Powered by {" "}
+        <ALink href="//kbshop.com/" title="Konnect Bio">
+          Konnect Bio
+        </ALink>
+        {" | "}
+        <ALink href="/terms" title="Terms of Service">
+          Terms of Service
+        </ALink>
+        {" | "}
+        <ALink href="/report" title="Report abuse">
+          Report Abuse
+        </ALink>
+        {publicRuntimeConfig.CONTACT_EMAIL && (
+          <>
+            {" | "}
+            <ALink
+              href={`mailto:${publicRuntimeConfig.CONTACT_EMAIL}`}
+              title="Contact us"
+            >
+              Contact us
+            </ALink>
+          </>
+        )}
+        .
+      </Text>
+    </ColCenter>
+  );
+};
+
+export default Footer;
